a2ac5443d6c87076755459776b4f0a80333840b5
Zicohh <zicobacker26@gmail.com>
2026-02-25 17:49:22 +0100

    changed golang to .NET updates routing

---
+++ NET-api/App/Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello world");
+
+app.Run();
